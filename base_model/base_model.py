b5e2a6257aedbf3b71aa8278ec2fba8edc6723b2
AlvaroC-ML <112903996+AlvaroC-ML@users.noreply.github.com>
2022-12-21 21:37:26 -0800

    Update base_model.py

--- base_model/base_model.py
+++ base_model/base_model.py
@@ -3,7 +3,8 @@
 
 from tensorflow.keras import Model 
 from tensorflow.keras.layers import Embedding, Dense
-from tensorflow import squeeze
+from tensorflow import squeeze, expand_dims, reshape
+from tensorflow.math import reduce_max
 
 from base_layer import base, RBFExpansion
 from spektral.layers.pooling.global_pool import GlobalAvgPool
@@ -17,28 +18,28 @@ class base_model(Model):
         rbf_dim=10
     ):
         super().__init__()
-        self.pool=GlobalAvgPool
+        self.pool=GlobalAvgPool()
         self.element_embedding=Embedding(num_elements, embedding, mask_zero=True)
-        self.element_mean=Embedding(num_elements, 1, mask_zero=True)
+        #self.element_mean=Embedding(num_elements, 1, mask_zero=True)
         self.embedding=embedding
 
         self.gnn=[
-            base(embedding)
+            base(embedding, aggregate='sum')
             for _ in range(message_passing)
         ]
         self.rbf=RBFExpansion(rbf_dim)
         self.dense_e=Dense(embedding)
-        self.dense_n=Dense(1)
+        self.dense_n=Dense(12)
 
     def call(self, inputs):
         x, a, e, i = inputs
+
         x=squeeze(x, axis=1)
         e=squeeze(e, axis=1)
 
         ##########
         # Preprocessing
         ##########
-        e_mean=self.element_mean(x)
         x=self.element_embedding(x)
         e=self.dense_e(self.rbf(e))
 
@@ -47,9 +48,13 @@ class base_model(Model):
         ##########
         for layer in self.gnn:
             x=layer([x, a, e])+x
-        
+
         ##########
         # Pooling
         ##########
-        x = self.dense_n(x)+e_mean
-        return self.pool(x, i)
+        x_s = self.dense_n(x)
+        output=self.pool([x_s, i])
+
+        batch_size = reduce_max(i) + 1
+
+        return reshape(output, [batch_size, 3, 4])
